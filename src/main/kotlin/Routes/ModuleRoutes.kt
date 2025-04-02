962c32ce6d8511c438585da0220e53ec42602f14
Emmanuel <emacal200901@gmail.com>
2025-04-02 00:21:26 -0600

    5 niveles

--- src/main/kotlin/Routes/ModuleRoutes.kt
+++ src/main/kotlin/Routes/ModuleRoutes.kt
@@ -13,7 +13,7 @@ import java.sql.ResultSet
 
 fun Route.moduleRoutes() {
     route("/modules") {
-        // Obtener todos los módulos con id, nombre y submódulos concatenados
+        // Obtener todos los módulos con id, nombre y submódulos anidados
         get("/get") {
             try {
                 val connection: Connection = Database.getConnection() ?: throw Exception("Connection error")
@@ -25,11 +25,20 @@ fun Route.moduleRoutes() {
                         sm.id AS idSubmodulo,
                         sm.nombreSubModulo AS nombreSubmodulo,
                         smHijo.id AS idSubmoduloHijo,
-                        smHijo.nombreSubModulo AS nombreSubmoduloHijo
+                        smHijo.nombreSubModulo AS nombreSubmoduloHijo,
+                        smHijo2.id AS idSubmoduloHijo2,
+                        smHijo2.nombreSubModulo AS nombreSubmoduloHijo2,
+                        smHijo3.id AS idSubmoduloHijo3,
+                        smHijo3.nombreSubModulo AS nombreSubmoduloHijo3,
+                        smHijo4.id AS idSubmoduloHijo4,
+                        smHijo4.nombreSubModulo AS nombreSubmoduloHijo4
                     FROM Modulo m
                     LEFT JOIN SubModulo sm ON sm.idModulo = m.id
                     LEFT JOIN SubModulo smHijo ON smHijo.idSubModuloPadre = sm.id
-                    ORDER BY m.id, sm.id, smHijo.id;
+                    LEFT JOIN SubModulo smHijo2 ON smHijo2.idSubModuloPadre = smHijo.id
+                    LEFT JOIN SubModulo smHijo3 ON smHijo3.idSubModuloPadre = smHijo2.id
+                    LEFT JOIN SubModulo smHijo4 ON smHijo4.idSubModuloPadre = smHijo3.id
+                    ORDER BY m.id, sm.id, smHijo.id, smHijo2.id, smHijo3.id, smHijo4.id;
                 """
 
                 val preparedStatement: PreparedStatement = connection.prepareStatement(query)
@@ -39,13 +48,18 @@ fun Route.moduleRoutes() {
 
                 while (resultSet.next()) {
                     val moduleId = resultSet.getInt("idModulo")
-                    val moduleName = resultSet.getString("nombreModulo")
+                    val moduleName = resultSet.getString("nombreModulo") ?: ""
                     val submoduleId = resultSet.getInt("idSubmodulo")
-                    val submoduleName = resultSet.getString("nombreSubmodulo")
+                    val submoduleName = resultSet.getString("nombreSubmodulo") ?: ""
                     val submoduleChildId = resultSet.getInt("idSubmoduloHijo")
-                    val submoduleChildName = resultSet.getString("nombreSubmoduloHijo")
+                    val submoduleChildName = resultSet.getString("nombreSubmoduloHijo") ?: ""
+                    val submoduleChildId2 = resultSet.getInt("idSubmoduloHijo2")
+                    val submoduleChildName2 = resultSet.getString("nombreSubmoduloHijo2") ?: ""
+                    val submoduleChildId3 = resultSet.getInt("idSubmoduloHijo3")
+                    val submoduleChildName3 = resultSet.getString("nombreSubmoduloHijo3") ?: ""
+                    val submoduleChildId4 = resultSet.getInt("idSubmoduloHijo4")
+                    val submoduleChildName4 = resultSet.getString("nombreSubmoduloHijo4") ?: ""
 
-                    // Si el módulo no existe en el mapa, lo creamos
                     val module = moduleMap.getOrPut(moduleId) {
                         Module(
                             moduleId = moduleId,
@@ -54,35 +68,38 @@ fun Route.moduleRoutes() {
                         )
                     }
 
-                    // Si hay un submódulo, lo agregamos a la lista
                     if (submoduleId != 0) {
                         val submodulesList = module.submodules as MutableList<Submodule1>
+                        val submodule = submodulesList.find { it.idSubmodulo == submoduleId }
+                            ?: Submodule1(submoduleId, submoduleName, mutableListOf()).also {
+                                submodulesList.add(it)
+                            }
 
-                        // Buscar si ya existe este submódulo
-                        val existingSubmodule = submodulesList.find { it.idSubmodulo == submoduleId }
-
-                        if (existingSubmodule == null) {
-                            // Si el submódulo no existe, lo agregamos
-                            submodulesList.add(
-                                Submodule1(
-                                    idSubmodulo = submoduleId,
-                                    name = submoduleName,
-                                    children = mutableListOf()
-                                )
-                            )
-                        }
-
-                        // Si hay un submódulo hijo, lo agregamos a la lista de hijos
                         if (submoduleChildId != 0) {
-                            val parentSubmodule = submodulesList.find { it.idSubmodulo == submoduleId }
+                            val subChild1 = submodule.children.find { it.idSubmodulo == submoduleChildId }
+                                ?: Submodule1(submoduleChildId, submoduleChildName, mutableListOf()).also {
+                                    submodule.children.add(it)
+                                }
 
-                            parentSubmodule?.children?.add(
-                                Submodule1(
-                                    idSubmodulo = submoduleChildId,
-                                    name = submoduleChildName,
-                                    children = mutableListOf()
-                                )
-                            )
+                            if (submoduleChildId2 != 0) {
+                                val subChild2 = subChild1.children.find { it.idSubmodulo == submoduleChildId2 }
+                                    ?: Submodule1(submoduleChildId2, submoduleChildName2, mutableListOf()).also {
+                                        subChild1.children.add(it)
+                                    }
+
+                                if (submoduleChildId3 != 0) {
+                                    val subChild3 = subChild2.children.find { it.idSubmodulo == submoduleChildId3 }
+                                        ?: Submodule1(submoduleChildId3, submoduleChildName3, mutableListOf()).also {
+                                            subChild2.children.add(it)
+                                        }
+
+                                    if (submoduleChildId4 != 0) {
+                                        subChild3.children.add(
+                                            Submodule1(submoduleChildId4, submoduleChildName4, mutableListOf())
+                                        )
+                                    }
+                                }
+                            }
                         }
                     }
                 }
